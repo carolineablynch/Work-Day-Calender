f43dfca34bd79ddb45268d5c2f81006d26394ba8
carolineablynch <73507944+carolineablynch@users.noreply.github.com>
2020-12-10 17:19:29 -0500

    creating function to dynamically highlight time blocks

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
         localStorage.setItem(timeStamp, userInput);
 
     })
+
+//highlight hour rows based on the time of day// 
+$(".user-input").each(function() {
+    var hourBlock = parseInt($(this).attr("id"));
+    if (hourBlock < currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    } else if (hourBlock === currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
