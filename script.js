84a9a49e717959c6019a525c613dc202956465e4
carolineablynch <73507944+carolineablynch@users.noreply.github.com>
2020-12-10 17:18:52 -0500

    adding function to save time and user input in local storage

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var currentHour = moment().hours();
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a")); 
+
+//Save user input in local storage // 
+    $(".saveBtn").on("click", function() {
+        var userInput = $(this).siblings(".user-input").val();
+        var timeStamp = $(this).siblings(".user-input").attr("id");
+        localStorage.setItem(timeStamp, userInput);
+
+    })
